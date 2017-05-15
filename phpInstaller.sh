bfc63967c9fc0f46f7c50257cf8abfc3dfb49053
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-15 20:29:14 +1000

    15/05/2017 - New update to the shell script again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -9,16 +9,18 @@ then
 fi
 
 # 1 argument or else no game
-if [ $# -ne 2 ]; then
-	printf "To use: `basename $0` <Random Number from Jenkins> <Reinstall Lamp? 0 or 1>"
+if [ $# -ne 3 ]; then
+	printf "To use: `basename $0` <Random Number from Jenkins> <Reinstall Lamp? 0 or 1> <secret mysql password>"
 	exit 1
 fi
 
 # save args and variables
 random=$1
 reinstall=$2
+secret=$3
 lamps=apache2,apache2-bin,apache2-data,apache2-mpm-prefork,libaio1,libapache2-mod-php5,libapr1,libaprutil1,libaprutil1-dbd-sqlite3,libaprutil1-ldap,libdbd-mysql-perl,libdbi-perl,libhtml-template-perl,libmysqlclient18,libterm-readkey-perl,mysql-client-5.5,mysql-client-core-5.5,mysql-common,mysql-server,mysql-server-5.5,mysql-server-core-5.5,php5-cli,php5-common,php5-json,php5-mysql,php5-readline,ssl-cert
 lampsInstalled="Yes"
+clamps=httpd,mysql-server,php,php-mysql
 errcount=0
 
 #check version of linux/unix
@@ -66,19 +68,19 @@ do
 		printf "Updating the $osver system before Lamp installation.\n"
 		apt-get update
 			#Check if lamp-server is installed already or not
-			printf "Checking if Lamp is installed already"
-			for i in $(printf $lamps | sed "s/,/ /g")
+			printf "Checking if Lamp is installed already.\n"
+			for i in $(echo $lamps | sed "s/,/ /g")
 			do
 				# call your procedure/other scripts here below
 				type "$i" >/dev/null 2>&1 || { printf >&2 "Lamp requires $i but it's not installed.\n"; errcount=$errcount+1; }
 			done
-			printf $errcount
+			printf "$errcount"
 			if [[ "$errcount" > 0 ]]
 			then
 				printf "Lamp is not yet installed.\n"
 				printf "Installing Lamp on $osver.\n"	
-				debconf-set-selections <<< 'mysql-server mysql-server/root_password password qwer1234'
-				debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password qwer1234'
+				debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret'
+				debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret'
 				apt-get install --assume-yes lamp-server^
 			elif [[ "$errcount" == 0 ]]
 			then
@@ -91,8 +93,47 @@ do
 	elif [[ "$lcosver" == *"centos"* ]]
 	then
 		printf "Attempting to install Lamp on $osver.\n"
-		###
-		exit 0
+		printf "Updating the $osver system before Lamp installation.\n"
+		yum -y update
+			#Check if lamp-server is installed already or not
+			printf "Checking if Lamp is installed already.\n"
+			for i in $(echo $clamps | sed "s/,/ /g")
+			do 
+				type "$i" >/dev/null 2>&1 || { printf >&2 "Lamp requires $i but it's not installed.\n"; errcount=$errcount+1; }
+			done
+			printf "$errcount"
+			if [[ "$errcount" > 0 ]]
+			then
+				printf "Lamp is not yet installed.\n"
+				printf "Installing Lap on $osver.\n"
+				
+				yum -y install httpd
+				yum -y install mysql-server
+				yum -y install php php-mysql
+		
+				#check ip of host
+				ipadd="ifconfig eth0 | grep inet | awk '{ print $2 }'"
+		
+				#set password for root of mysql
+				echo  "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$secret');" | mysql -u root
+
+				#Start services
+				service httpd start
+				service mysqld start
+
+
+				#Configure to automatically run on boot
+				chkconfig httpd on
+				chkconfig mysqld on
+				###
+				exit 0
+			elif [[ "$errcount" == 0 ]]
+                        then
+                                printf "Lamp is already installed\n"
+                                printf "$reinstall"
+                        else
+                                printf "testing"
+			fi
 	else
 		printf "Figuring out hot to install this...\n"
 		exit 0
