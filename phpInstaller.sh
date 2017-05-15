035e4c315d997138d5a4cc59521655326d1993eb
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-15 22:37:11 +1000

    15/05/2017 - New update to the shell script again and again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -71,7 +71,7 @@ do
 			for i in $(echo $lamps | sed "s/,/ /g")
 			do
 				# call your procedure/other scripts here below
-				type "$i"
+				type "$i">/dev/null 2>&1 || { printf >&2 "Lamp requires $i but it's not installed.\n"; errcount="$errcount+1"; }
 			done
 			printf "Number of components not installed: $errcount\n"
 			if [[ "$errcount" > 0 ]]
