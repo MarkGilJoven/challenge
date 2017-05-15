c9f5f91d0809c9af18f18f9c052468a522aaf4b0
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-15 21:57:29 +1000

    15/05/2017 - New update to the shell script again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -4,25 +4,22 @@
 IAM=`whoami`
 if [ "$IAM" != "root" ]
 then
-  printf "I'm sorry: you must be a root to run this script"
+  echo "I'm sorry: you must be a root to run this script"
   exit 1
 fi
 
 # 1 argument or else no game
-if [ $# -ne 3 ]; then
-	printf "To use: `basename $0` <Random Number from Jenkins> <Reinstall Lamp? 0 or 1> <secret mysql password>"
+if [ $# -ne 2 ]; then
+	printf "To use: `basename $0` <Random Number from Jenkins> <Reinstall Lamp? 0 or 1>"
 	exit 1
 fi
 
 # save args and variables
 random=$1
 reinstall=$2
-secret=$3
 lamps=apache2,apache2-bin,apache2-data,apache2-mpm-prefork,libaio1,libapache2-mod-php5,libapr1,libaprutil1,libaprutil1-dbd-sqlite3,libaprutil1-ldap,libdbd-mysql-perl,libdbi-perl,libhtml-template-perl,libmysqlclient18,libterm-readkey-perl,mysql-client-5.5,mysql-client-core-5.5,mysql-common,mysql-server,mysql-server-5.5,mysql-server-core-5.5,php5-cli,php5-common,php5-json,php5-mysql,php5-readline,ssl-cert
 lampsInstalled="Yes"
-clamps=httpd,mysql-server,php,php-mysql
 errcount=0
-cerrcount=0
 
 #check version of linux/unix
 osver="$(cat /etc/os-release | grep '^NAME=' | awk -F"=" '{print $2}')"
@@ -36,15 +33,14 @@ work_directory=`mktemp -d "$directory/tmp.$random.XXXXX"`
 
 # check if tmp directory was created
 if [[ ! "$work_directory" || ! -d "$work_directory" ]]; then
-  printf "Could not create temp directory"
+  echo "Could not create temp directory"
   exit 1
 fi
 
 # deletes the temp directory
 function cleanup {      
   rm -rf "$work_directory"
-  rm -rf "/tmp/`basename $0`
-  print "Deleted temp working directory $work_directory\n"
+  echo "Deleted temp working directory $work_directory"
 }
 
 # register the cleanup function to be called on the EXIT signal
@@ -58,7 +54,6 @@ do
 	then
 		printf "\n$msgerr\n"
 	fi
-	# Check if os version is null
 
 	if [[ "$lcosver" == null ]]
 	then
@@ -70,13 +65,13 @@ do
 		printf "Updating the $osver system before Lamp installation.\n"
 		apt-get update
 			#Check if lamp-server is installed already or not
-			printf "Checking if Lamp is installed already.\n"
-			for i in $(echo $lamps | sed "s/,/ /g")
+			printf "Checking if Lamp is installed already"
+			for i in $(printf $lamps | sed "s/,/ /g")
 			do
 				# call your procedure/other scripts here below
-				type "$i" >/dev/null 2>&1 || { printf >&2 "Lamp requires $i but it's not installed.\n"; errcount=$errcount+1; }
+				type "$i" >/dev/null 2>&1 || { printf >&2 "Lamp requires $i but it's not installed.\n"; errcount=$errcount + 1; }
 			done
-			printf "$errcount"
+			printf $errcount
 			if [[ "$errcount" > 0 ]]
 			then
 				printf "Lamp is not yet installed.\n"
@@ -95,25 +90,7 @@ do
 	elif [[ "$lcosver" == *"centos"* ]]
 	then
 		printf "Attempting to install Lamp on $osver.\n"
-		printf "Updating the $osver system before Lamp installation.\n"
-		yum -y update
-			#Check if lamp-server is installed already or not
-			printf "Checking if Lamp is installed already.\n"
-			#for j in $(echo $clamps | sed "s/,/ /g")
-			#do 
-			#	type "$j" >/dev/null 2>&1 || { printf >&2 "Lamp requires $j but it's not installed.\n"; cerrcount=$cerrcount+1; }
-			#done
-			#printf "$cerrcount"
-			#if [[ "$cerrcount" > 0 ]]
-			#then
-				printf "Lamp is not yet installed.\n"
-			#elif [[ "$cerrcount" == 0 ]]
-			#then
-			#	printf "Lamp is already installed\n"
-				printf "$reinstall"
-                        #else
-                        #        printf "testing"
-			#fi
+		###
 		exit 0
 	else
 		printf "Figuring out hot to install this...\n"
