f51631456d472febd389e4be9f76316915ec775d
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-16 13:56:48 +1000

    16/05/2017 - New update to the shell script again and again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -51,6 +51,31 @@ cleanup() {
   echo "Deleted temp working directory $work_directory"
 }
 
+installphp() {
+printf "Installing Lamp on $osver.\n"
+python -mplatform | grep -qi $osver && debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret' && debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret' || mysql < "UPDATE mysql.user SET Password=PASSWORD('${secret}') WHERE User='root'; DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;"
+python -mplatform | grep -qi $osver && apt-get install --assume-yes lamp-server^ || yum -y install httpd mariadb mariadb-server php php-mysql && chkconfig httpd on && chkconfig httpd on
+}
+
+reinstallphp() {
+printf "Stopping services.\n"
+	if [[ $lcosver == *"ubuntu"* ]]
+	then
+		for i in ${lamps//,/ }
+		do
+			serviceCommand $i stop
+		done
+	else
+		for i in ${clamps//,/ }
+		do
+			serviceCommand $i stop
+		done
+	fi
+printf "Uninstalling Lamp.\n"
+python -mplatform | grep -i $osver && apt-get -y purge apache2 php5-cli apache2-mpm-prefork apache2-utils apache2.2-common libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client mysql-common mysql-server php5-common php5-mysql phpmyadmin && sudo apt-get autoremove || yum -y remove httpd httpd-devel httpd-manual httpd-tools mod_auth_kerb mod_auth_mysql mod_auth_pgsql mod_authz_ldap mod_dav_svn mod_dnssd mod_nss mod_perl mod_revocator mod_ssl mod_wsgi php php-cli php-common php-gd php-ldap php-mysql php-odbc php-pdo php-pear php-pecl-apc php-pecl-memcache php-pgsql php-soap php-xml php-xmlrpc
+installphp
+}
+
 # register the cleanup function to be called on the EXIT signal
 trap cleanup EXIT
 
@@ -101,17 +126,7 @@ do
 				printf "Lamp is not yet installed.\n"
 				if [[ $reinstall == "true" ]]
 				then
-					printf "Reinstalling Lamp on $osver.\n"
-					printf "Stopping services.\n"
-					for i in ${lamps//,/ }
-					do
-						serviceCommand $i stop
-					done
-					printf "Uninstalling Lamp.\n"
-					python -mplatform | grep -i $osver && apt-get -y purge apache2 php5-cli apache2-mpm-prefork apache2-utils apache2.2-common libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client mysql-common mysql-server php5-common php5-mysql phpmyadmin && sudo apt-get autoremove || yum -y remove httpd httpd-devel httpd-manual httpd-tools mod_auth_kerb mod_auth_mysql mod_auth_pgsql mod_authz_ldap mod_dav_svn mod_dnssd mod_nss mod_perl mod_revocator mod_ssl mod_wsgi php php-cli php-common php-gd php-ldap php-mysql php-odbc php-pdo php-pear php-pecl-apc php-pecl-memcache php-pgsql php-soap php-xml php-xmlrpc 
-					printf "Installing Lamp on $osver.\n"
-					python -mplatform | grep -qi $osver && debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret' && debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret' || mysqladmin -u root -p '' password '$secret'
-					python -mplatform | grep -qi $osver && apt-get install --assume-yes lamp-server^ || yum -y install httpd mariadb mariadb-server php php-mysql && chkconfig httpd on && chkconfig httpd on
+					reinstallphp
 					printf "Move helloworld.php to /var/www/html .\n"
 					mv /tmp/helloworld.php /var/www/html || { printf 'Moving helloworld.php failed.' ; exit 1; }
 					printf "Restarting Apache."
@@ -122,8 +137,7 @@ do
 						serviceCommand httpd restart || { printf 'Apache restart failed.' ; exit 1; }
 					fi		
 				else
-					printf "Installing Lamp on $osver.\n"
-					python -mplatform | grep -qi $lcosver && apt-get install --assume-yes lamp-server^ || yum -y install httpd mariadb mariadb-server php php-mysql && chkconfig httpd on && chkconfig httpd on
+					installphp
 					printf "Move helloworld.php to /var/www/html .\n"
 					mv /tmp/helloworld.php /var/www/html || { printf 'Moving helloworld.php failed.' ; exit 1; }
 					printf "Restarting Apache."
@@ -136,15 +150,22 @@ do
 				fi
 			else
 				printf "Lamp is already installed.\n"
+				if [[ $reinstall == "true" ]]
+				then
+					reinstallphp
+                                        printf "Move helloworld.php to /var/www/html .\n"
+                                        mv /tmp/helloworld.php /var/www/html || { printf 'Moving helloworld.php failed.' ; exit 1; }
+                                        printf "Restarting Apache."
+                                        if [[ $lcosver == *"ubuntu"* ]]
+                                        then
+                                                serviceCommand apache2 restart || { printf 'Apache restart failed.' ; exit 1; }
+                                        else
+                                                serviceCommand httpd restart || { printf 'Apache restart failed.' ; exit 1; }
+                                        fi
+				else
+				printf "Thank you.\n"
+				fi
 			fi
 		exit 0
 	fi
-mysql --user=root <<_EOF_
-  UPDATE mysql.user SET Password=PASSWORD('${secret}') WHERE User='root';
-  DELETE FROM mysql.user WHERE User='';
-  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-  DROP DATABASE IF EXISTS test;
-  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-  FLUSH PRIVILEGES;
-_EOF_
 done
