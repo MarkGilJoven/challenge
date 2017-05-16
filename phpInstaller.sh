b7e33c80252457568227f285ec575f89940be4ee
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-16 18:26:59 +1000

    16/05/2017 - New update to the shell script again and again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+# Check if iam root or else no game 
+IAM=`whoami`
+if [ "$IAM" != "root" ]
+then
+  echo "I'm sorry: you must be a root to run this script"
+  exit 1
+fi
+#check version of linux/unix
+osver="$(cat /etc/os-release | grep '^NAME=' | awk -F"=" '{print $2}')"
+lcosver="${osver,,}"
 
-if [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ]
+if [ "$lcosver" == *"ubuntu"* ] || [ "$lcosver" == *"debian"* ]
 then
     apt-get -y install mysql-server mysql-client mysql-workbench libmysqld-dev;
     apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt phpmyadmin;
@@ -8,7 +18,7 @@ then
     printf "<?php\nheader("Content-Type: text/plain"); echo "Hello, world!"\n?>" > /var/www/html/hello.php;
     service apache2 restart;
 
-elif [ "`lsb_release -is`" == "CentOS" ] || [ "`lsb_release -is`" == "RedHat" ]
+elif [ "$lcosver" == *"centos"* ] || [ "$lcosver" == *"redhat"* ]
 then
     yum -y install httpd mysql-server mysql-devel php php-mysql php-fpm;
     yum -y install epel-release phpmyadmin rpm-build redhat-rpm-config;
