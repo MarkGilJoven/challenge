bb4c177b70bece375727158bc17482a1d694acc6
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-16 18:19:36 +1000

    16/05/2017 - New update to the shell script again and again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -5,7 +5,7 @@ then
     apt-get -y install mysql-server mysql-client mysql-workbench libmysqld-dev;
     apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt phpmyadmin;
     chmod 755 -R /var/www/;
-    printf "<?php\nheader("Content-Type: text/plain"); echo "Hello, world!\n?>" > /var/www/html/hello.php;
+    printf "<?php\nheader("Content-Type: text/plain"); echo "Hello, world!"\n?>" > /var/www/html/hello.php;
     service apache2 restart;
 
 elif [ "`lsb_release -is`" == "CentOS" ] || [ "`lsb_release -is`" == "RedHat" ]
@@ -15,7 +15,7 @@ then
     yum -y install mysql-community-release-el7-5.noarch.rpm proj;
     yum -y install tinyxml libzip mysql-workbench-community;
     chmod 755 -R /var/www/;
-    printf "<?php\nheader("Content-Type: text/plain"); echo "Hello, world!\n?>" > /var/www/html/hello.php;
+    printf "<?php\nheader("Content-Type: text/plain"); echo "Hello, world!"\n?>" > /var/www/html/hello.php;
     service mysqld restart;
     service httpd restart;
     chkconfig httpd on;
