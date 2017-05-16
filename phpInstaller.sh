07da74e04449e65a6ba83aa1ea99b63bfba01106
Mark Gil Joven <markgiljoven@Marks-MBP.lan>
2017-05-16 10:40:43 +1000

    16/05/2017 - New update to the shell script again and again...

--- phpInstaller.sh
+++ phpInstaller.sh
@@ -24,7 +24,7 @@ serviceCommand() {
 random=$1
 reinstall=$2
 secret=$3
-lamps=apache2,mysql-server,php
+lamps=apache2,mysql
 lampsInstalled="Yes"
 errcount=0
 
@@ -67,11 +67,11 @@ do
 	then
 		printf "The OS version couldn't be found.  Exiting prematurely."
 		exit 1
-	elif [[ "$lcosver" == *"ubuntu"* ]]
+	elif [[ "$lcosver" == *"ubuntu"* || "$lcosver" == *"centos"* ]]
 	then
 		printf "Attempting to install Lamp on $osver.\n"
 		printf "Updating the $osver system before Lamp installation.\n"
-		apt-get update
+		python -mplatform | grep -qi $lcosver && apt-get update --assume-yes || yum -y update && yum -y install epel-release
 			#Check if lamp-server is installed already or not
 			printf "Checking if Lamp is installed already.\n"
 			#for i in $(echo $lamps | sed "s/,/ /g")
@@ -84,6 +84,7 @@ do
 					errcount=$((errcount+1))
 				fi
 			done
+			printf "\nCHECK $errcount\n"
 			if [[ $errcount > 0 ]]
 			then
 				printf "Lamp is not yet installed.\n"
@@ -96,27 +97,26 @@ do
 						serviceCommand $i stop
 					done
 					printf "Uninstalling Lamp.\n"
-					apt-get -y purge apache2 php5-cli apache2-mpm-prefork apache2-utils apache2.2-common libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client mysql-common mysql-server php5-common php5-mysql phpmyadmin && sudo apt-get autoremove
+					python -mplatform | grep -qi $lcosver && apt-get -y purge apache2 php5-cli apache2-mpm-prefork apache2-utils apache2.2-common libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client mysql-common mysql-server php5-common php5-mysql phpmyadmin && sudo apt-get autoremove || yum -y remove httpd httpd-devel httpd-manual httpd-tools mod_auth_kerb mod_auth_mysql mod_auth_pgsql mod_authz_ldap mod_dav_svn mod_dnssd mod_nss mod_perl mod_revocator mod_ssl mod_wsgi php php-cli php-common php-gd php-ldap php-mysql php-odbc php-pdo php-pear php-pecl-apc php-pecl-memcache php-pgsql php-soap php-xml php-xmlrpc 
 					printf "Installing Lamp on $osver.\n"
-					debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret'
-					debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret'
-					apt-get install --assume-yes lamp-server^
-					apt-get install --assume-yes libapache2-mod-php
+					python -mplatform | grep -qi $lcosver && debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret' && debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret' || mysqladmin -u root password $secret
+					python -mplatform | grep -qi $lcosver && apt-get install --assume-yes lamp-server^ || yum -y install httpd mysql-server php php-mysql && chkconfig httpd on && chkconfig httpd on
+					printf "Move helloworld.php to /var/www/html .\n"
+					mv /tmp/helloworld.php /var/www/html || { printf 'Moving helloworld.php failed.' ; exit 1; }
+					printf "Restarting Apache."
+					serviceCommand apache2 restart || { printf 'Apache restart failed.' ; exit 1; }
 				else
 					printf "Installing Lamp on $osver.\n"
-					debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret'
-                                        debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret'
-                                        apt-get install --assume-yes lamp-server^
-					apt-get install --assume-yes libapache2-mod-php
+					python -mplatform | grep -qi $lcosver && debconf-set-selections <<< 'mysql-server mysql-server/root_password password $secret' && debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $secret' || mysqladmin -u root password $secret
+					python -mplatform | grep -qi $lcosver && apt-get install --assume-yes lamp-server^ || yum -y install httpd mysql-server php php-mysql && chkconfig httpd on && chkconfig httpd on
+					printf "Move helloworld.php to /var/www/html .\n"
+					mv /tmp/helloworld.php /var/www/html || { printf 'Moving helloworld.php failed.' ; exit 1; }
+					printf "Restarting Apache."
+					serviceCommand apache2 restart || { printf 'Apache restart failed.' ; exit 1; }
 				fi
 			else
 				printf "Lamp is already installed.\n"
 			fi
-		exit 0
-	elif [[ "$lcosver" == *"centos"* ]]
-	then
-		printf "Attempting to install Lamp on $osver.\n"
-		###
 		exit 0
 	else
 		printf "Figuring out hot to install this...\n"
